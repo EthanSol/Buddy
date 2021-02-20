e291c5410668028e4181ec6bc0180887ca3d7105
EthanSol <ethanisrsolomon@gmail.com>
2021-02-20 08:12:37 -0800

    Added firebase db and hosting

---
+++ components/BuddyBanner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image, Text} from 'react-native';
+
+export default function BuddyBanner() {
+    return (
+        <View>
+            <Text>Banner</Text>
+        </View>
+    );
+}
